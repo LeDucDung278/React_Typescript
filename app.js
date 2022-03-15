584e9b38d690842c66c6d0ed8c3d95b98524295a
ledung <dungldc6ts42020@gmail.com>
2022-03-15 18:41:39 +0700

    tsc

---
+++ app.js
@@ -0,0 +1,4 @@
+function sum(numA, numB) {
+    return numA + numB;
+}
+console.log(sum(10, 20));
---
+++ app.ts
@@ -0,0 +1,4 @@
+function sum(numA:number, numB:number) {
+    return numA + numB;
+}
+console.log(sum(10,20));
